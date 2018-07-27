b12a7f7675edb45231a30759f019cc37ae1daeeb
Mujibur Rehman <mujibur@sportzinteractive.com>
2018-07-27 16:35:45 +0530

    product search completed

--- src/app/pages/admin-products/admin-products.component.ts
+++ src/app/pages/admin-products/admin-products.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
 	selector: 'app-admin-products',
 	templateUrl: './admin-products.component.html',
 	styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-	products$: any;
+	// products$: any;
+	products: any[];
+	filteredProducts: any[];
+	subscription: Subscription;
 
 	constructor(
 		private productService: ProductService
 	) {}
 
 	ngOnInit() {
+		// this.products$ = this.productService.getAll();
+		this.subscription = this.productService.getAll().subscribe( p => {
+			this.filteredProducts = this.products = p;
+		} );
+	}
 
-		this.products$ = this.productService.getAll();
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
+
+	filter( query: string ) {
 		
+		this.filteredProducts = ( query ) ?
+			this.products.filter( p => {
+				return p.payload.val().title.toLowerCase().includes( query.toLowerCase() )
+			} ) :
+			this.products;
+
 	}
 
 }
